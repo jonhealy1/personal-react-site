688d9fe6245a894c3d4a902a73d7f3b429287a2f
jonhealy1 <jonathan.d.healy@gmail.com>
2024-12-12 20:13:36 +0800

    add locations, circle sizes, colors

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,6 +7,7 @@ const locations = [
   { name: "Antalya, Turkey", coords: [30.7133, 36.8969], daysSpent: 10 },
   { name: "Ankara, Turkey", coords: [32.8597, 39.9208], yearsSpent: 2 },
   { name: "Athens, Greece", coords: [23.7275, 37.9838], daysSpent: 6 },
+  { name: "Banff, Canada", coords: [-115.5708, 51.1784], daysSpent: 25 },
   { name: "Bangkok, Thailand", coords: [100.5018, 13.7563], daysSpent: 2 },
   { name: "Bocas del Toro, Panama", coords: [-82.2516, 9.3339], daysSpent: 10 },
   {
@@ -16,6 +17,7 @@ const locations = [
   },
   { name: "Calgary, Canada", coords: [-114.0719, 51.0447], daysSpent: 8 },
   { name: "Cancún, Mexico", coords: [-86.8475, 21.1619], daysSpent: 1 },
+  { name: "Cappadocia, Turkey", coords: [34.8467, 38.6431], daysSpent: 7 },
   { name: "Cartagena, Colombia", coords: [-75.4794, 10.391], daysSpent: 30 },
   { name: "Corfu Island, Greece", coords: [19.9211, 39.6243], daysSpent: 5 },
   { name: "Fethiye, Turkey", coords: [29.1168, 36.6219], daysSpent: 3 },
@@ -26,16 +28,22 @@ const locations = [
   },
   { name: "Halifax, Canada", coords: [-63.5752, 44.6488], daysSpent: 7 },
   { name: "Hoi An, Vietnam", coords: [108.3275, 15.8801], daysSpent: 7 },
+  { name: "Hong Kong, China", coords: [114.1694, 22.3193], daysSpent: 3 },
   { name: "Istanbul, Turkey", coords: [28.9784, 41.0082], daysSpent: 6 },
   { name: "Izmir, Turkey", coords: [27.1428, 38.4237], daysSpent: 5 },
   { name: "Kalkan, Turkey", coords: [29.4183, 36.2641], daysSpent: 90 },
+  { name: "Kelvington, Canada", coords: [-103.5217, 52.1635], yearsSpent: 1 },
   { name: "Konya, Turkey", coords: [32.4845, 37.8746], daysSpent: 5 },
   { name: "Kota Kinabalu, Malaysia", coords: [116.0724, 5.978], daysSpent: 7 },
   { name: "Langkawi, Malaysia", coords: [99.7985, 6.3628], daysSpent: 12 },
+  { name: "Lethbridge, Canada", coords: [-112.8418, 49.6935], daysSpent: 10 },
   { name: "Macau, China", coords: [113.5439, 22.1987], yearsSpent: 1.5 },
   { name: "Malta", coords: [14.3754, 35.8997], daysSpent: 7 },
+  { name: "Medicine Hat, Canada", coords: [-110.6766, 50.0405], daysSpent: 12 },
+  { name: "Mexico City, Mexico", coords: [-99.1332, 19.4326], daysSpent: 3 },
   { name: "Moalboal, Philippines", coords: [123.3983, 9.9472], daysSpent: 8 },
   { name: "Montreal, Canada", coords: [-73.5673, 45.5017], yearsSpent: 1 },
+  { name: "Moose Jaw, Canada", coords: [-105.5345, 50.3923], daysSpent: 15 },
   { name: "Nanaimo, Canada", coords: [-123.9401, 49.1659], yearsSpent: 3 },
   { name: "Nelson, Canada", coords: [-117.2948, 49.4934], daysSpent: 40 },
   { name: "North Cyprus", coords: [33.3333, 35.3333], daysSpent: 14 },
@@ -43,6 +51,9 @@ const locations = [
   { name: "Osoyoos, Canada", coords: [-119.4687, 49.0323], yearsSpent: 2 },
   { name: "Panama City, Panama", coords: [-79.5167, 8.9833], daysSpent: 10 },
   { name: "Phoenix, USA", coords: [-112.074, 33.4484], daysSpent: 6 },
+  { name: "Port Alberni, Canada", coords: [-124.8022, 49.2338], daysSpent: 1 },
+  { name: "Port Hardy, Canada", coords: [-127.492, 50.7197], daysSpent: 3 },
+  { name: "Portland, USA", coords: [-122.6765, 45.5231], daysSpent: 2 },
   {
     name: "Prince Albert, Canada",
     coords: [-105.7495, 53.2033],
@@ -54,33 +65,49 @@ const locations = [
     daysSpent: 15,
   },
   { name: "Regina, Canada", coords: [-104.6189, 50.4452], yearsSpent: 7 },
+  { name: "Rhodes Island, Greece", coords: [28.2282, 36.4349], daysSpent: 14 },
+  {
+    name: "Saint John, New Brunswick, Canada",
+    coords: [-66.057, 45.2733],
+    daysSpent: 6,
+  },
   { name: "San Diego, USA", coords: [-117.1611, 32.7157], daysSpent: 26 },
   {
     name: "San Blas Islands, Panama",
     coords: [-78.9767, 9.5721],
     daysSpent: 30,
   },
+  { name: "San Francisco, USA", coords: [-122.4194, 37.7749], daysSpent: 5 },
+  {
+    name: "San Salvador, El Salvador",
+    coords: [-89.2182, 13.6929],
+    daysSpent: 1,
+  },
   {
     name: "Santa Teresa, Costa Rica",
     coords: [-85.1605, 9.6477],
     daysSpent: 7,
   },
-  { name: "Sarandë, Albania", coords: [19.9836, 39.8757], daysSpent: 4 },
+  { name: "Sarandë, Albania", coords: [19.9836, 39.8757], daysSpent: 5 },
   { name: "Saskatoon, Canada", coords: [-106.6345, 52.1332], yearsSpent: 12 },
+  { name: "Seattle, USA", coords: [-122.3321, 47.6062], daysSpent: 5 },
   { name: "Sofia, Bulgaria", coords: [23.3219, 42.6977], daysSpent: 4 },
+  { name: "Tofino, Canada", coords: [-125.8942, 49.1529], daysSpent: 20 },
   { name: "Toronto, Canada", coords: [-79.3832, 43.6532], daysSpent: 22 },
   { name: "Tulum, Mexico", coords: [-87.4667, 20.2117], daysSpent: 9 },
   { name: "Vancouver, Canada", coords: [-123.1216, 49.2827], yearsSpent: 10 },
+  { name: "Vernon, Canada", coords: [-119.271, 50.267], daysSpent: 4 },
   { name: "Victoria, Canada", coords: [-123.3656, 48.4284], yearsSpent: 5 },
   { name: "Winnipeg, Canada", coords: [-97.1375, 49.8951], daysSpent: 5 },
+  { name: "Zonguldak, Turkey", coords: [31.7917, 41.4564], daysSpent: 3 },
 ];
 
 const MapLibreComponent = () => {
   const mapContainerRef = useRef(null);
 
   const calculateVisitedCircleSize = (days) => {
-    const maxDays = 90; // Max days for visited
-    const baseSize = window.innerWidth < 768 ? 7 : 10; // Smaller base for small screens
+    const maxDays = 50; // Max days for visited
+    const baseSize = window.innerWidth < 768 ? 9 : 11; // Smaller base for small screens
     const maxSize = window.innerWidth < 768 ? 14 : 20; // Smaller max size for small screens
     return Math.min(
       baseSize + (days / maxDays) * (maxSize - baseSize),
@@ -90,7 +117,7 @@ const MapLibreComponent = () => {
 
   const calculateLivedCircleSize = (years) => {
     const maxYears = 20; // Example maximum years for scaling
-    const baseSize = window.innerWidth < 768 ? 11 : 13; // Smaller base for small screens
+    const baseSize = window.innerWidth < 768 ? 12 : 14; // Smaller base for small screens
     const maxSize = window.innerWidth < 768 ? 25 : 40; // Smaller max size for small screens
     return Math.min(
       baseSize + (years / maxYears) * (maxSize - baseSize),
@@ -99,12 +126,12 @@ const MapLibreComponent = () => {
   };
 
   const calculateVisitedGradientColor = (days) => {
-    const maxDays = 90;
+    const maxDays = 60;
     const normalized = Math.min(days / maxDays, 1);
 
-    const r = Math.round(173 * (1 - normalized));
-    const g = Math.round(216 * (1 - normalized));
-    const b = Math.round(230 + 25 * normalized);
+    const r = Math.round(150 * (1 - normalized)); // Reduced from 173
+    const g = Math.round(200 * (1 - normalized)); // Reduced from 216
+    const b = Math.round(220 + 25 * normalized); // Reduced from 230 for a slightly darker blue
 
     return `rgb(${r}, ${g}, ${b})`;
   };
